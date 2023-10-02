e675a21f1a2ff735fdbf79c2441e9f81a056ae8e
kimhongrae <khr0690@gmail.com>
2023-10-02 15:32:13 +0900

    feat: 대시보드 왼쪽보드, 오늘 할 일 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import InternalMainpage from './Pages/InternalPage/InternalMainpage';
+import logo from "./logo.svg";
+import "./App.css";
+import InternalMainpage from "./Pages/InternalPage/InternalMainpage";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import EditNoticeMainpage from "./Pages/NoticeBoardPage/EditNoticePage/EditNoticeMainpage";
 import MakingNoticeMainpage from "./Pages/NoticeBoardPage/MakingNoticePage/MakingNoticeMainpage";
 import PlanNoticeMainpage from "./Pages/NoticeBoardPage/PlanNoticePage/PlanNoticeMainpage";
 import LoginPage from "./Pages/Login/LoginPage";
 import SignInPage from "./Pages/Login/SignInPage";
+import Manage from "./Pages/InternalPage/Dashboard/Manage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<InternalMainpage />}/>
-        <Route path="/EditMain" element={<EditNoticeMainpage />}/>
-        <Route path="/MakingMain" element={<MakingNoticeMainpage />}/>
-        <Route path="/PlanMain" element={<PlanNoticeMainpage />}/>
-        <Route path="/LoginPage" element={<LoginPage />}/>
-        <Route path="/SignInPage" element={<SignInPage />}/>
+        <Route path="/" element={<InternalMainpage />} />
+        <Route path="/EditMain" element={<EditNoticeMainpage />} />
+        <Route path="/MakingMain" element={<MakingNoticeMainpage />} />
+        <Route path="/PlanMain" element={<PlanNoticeMainpage />} />
+        <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/SignInPage" element={<SignInPage />} />
+        <Route path="/manage" element={<Manage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Pages/InternalPage/Dashboard/Today.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin-top: -60px;
+`;
+const Subtitle = styled.text`
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
+
+const ListItem = styled.div``;
+
+function Today() {
+  const [todayEvents, setTodayEvents] = useState([]);
+
+  useEffect(() => {
+    const storedEvents = JSON.parse(localStorage.getItem("events") || "[]");
+    const todayDate = new Date().toISOString().substr(0, 10); // YYYY-MM-DD format
+
+    const filteredEvents = storedEvents.filter((e) => e.date === todayDate);
+    setTodayEvents(filteredEvents);
+  }, []);
+
+  return (
+    <Container>
+      <Subtitle>Today</Subtitle>
+      {todayEvents.length === 0 ? (
+        <p>오늘의 일정이 없습니다.</p>
+      ) : (
+        todayEvents.map((event) => (
+          <ListItem key={event.date}>{event.event}</ListItem>
+        ))
+      )}
+    </Container>
+  );
+}
+
+export default Today;
