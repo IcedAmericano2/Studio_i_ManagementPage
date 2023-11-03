bd3b603e190caf1abbf02b047551978550b6c5a3
kimhongrae <khr0690@gmail.com>
2023-11-03 21:12:00 +0900

    feat: 완료된 프로젝트 삭제 버튼 구현

--- src/Pages/ProjectPage/FinishProject.js
+++ src/Pages/ProjectPage/FinishProject.js
@@ -56,20 +56,64 @@ const LabelArea = styled.div`
   text-align: center;
   color: white;
 `;
+const DeleteButton = styled.button`
+  background-color: #363636;
+  border-radius: 32px;
+  border: none;
+  outline: none;
+  color: white;
+  padding: 2px 16px;
+  margin: 4px;
+
+  &:hover {
+    background-color: black;
+  }
+`;
 
 function FinishProject() {
   const [projects, setProjects] = useState([]);
+
+  const refresh = () => {
+    // Use setTimeout for a simple refresh delay, could be replaced with state-based re-rendering if needed
+    setTimeout(function () {
+      window.location.reload();
+    }, 100);
+  };
+
+  const handleDeleteClick = async (e, projectId) => {
+    e.stopPropagation();
+    const isConfirmed = window.confirm("프로젝트를 삭제하시겠습니까?");
+    if (!isConfirmed) return;
+
+    try {
+      const response = await projectApi.deleteProject(projectId);
+      if (response.data && response.data.success) {
+        // If deletion is successful, filter out the deleted project and update the state
+        const updatedProjects = projects.filter(
+          (project) => project.projectId !== projectId
+        );
+        setProjects(updatedProjects);
+        alert("프로젝트가 삭제 처리 되었습니다.");
+      } else {
+        // Handle the case where the API did not return a success message
+        alert("프로젝트 삭제에 실패했습니다. " + response.data.message);
+      }
+    } catch (error) {
+      console.error("Error deleting the project:", error);
+      alert("프로젝트 삭제에 실패했습니다.");
+    }
+  };
 
   useEffect(() => {
     const fetchProjects = async () => {
       try {
         const response = await projectApi.getProjectList();
         if (response.data && response.data.success === false) {
-          if(response.data.code === 7000){
+          if (response.data.code === 7000) {
             //OngoingProject에서 해줌.
-          }else if(response.data.code === 7001){
+          } else if (response.data.code === 7001) {
             sessionStorage.removeItem("login-token");
-            delete axios.defaults.headers.common['Authorization'];
+            delete axios.defaults.headers.common["Authorization"];
           }
           return;
         }
@@ -114,7 +158,13 @@ function FinishProject() {
               </td>
               <td>{project.name}</td>
               <td>{project.description}</td>
-              <td></td>
+              <td>
+                <DeleteButton
+                  onClick={(e) => handleDeleteClick(e, project.projectId)}
+                >
+                  삭제
+                </DeleteButton>
+              </td>
             </tr>
           ))}
         </tbody>
@@ -122,81 +172,4 @@ function FinishProject() {
     </AppContainer>
   );
 }
-const projectData = [
-  {
-    id: 1,
-    date: "2020",
-    name: "벨리곰",
-    description: "롯데홈쇼핑메라 콘텐츠",
-    etc: "youtube link",
-  },
-  {
-    id: 2,
-    date: "2021",
-    name: "아작아작:아이돌 작사 아이돌 작곡",
-    description: "케이(Kei)로 본격적인 ‘봄 시즌 송’ 제작기.",
-    etc: "youtube link",
-  },
-  {
-    id: 3,
-    date: "2021",
-    name: "김용명의 쏠쏠한 이중생활",
-    description: "나만의 활 즐기기에 나선다!",
-    etc: "youtube link",
-  },
-  {
-    id: 4,
-    date: "2021",
-    name: "주크박스2",
-    description:
-      "K-CM송을 차린 KCM이 광고주를 위한 최고의 CM송을  만들어주는 콘텐츠",
-    etc: "youtube link",
-  },
-  {
-    id: 5,
-    date: "2022",
-    name: "국힘겜탑",
-    description: "롯데홈쇼핑메라 콘텐츠",
-    etc: "youtube link",
-  },
-  {
-    id: 6,
-    date: "2022",
-    name: "쓰봉크럽",
-    description: "케이(Kei)로 본격적인 ‘봄 시즌 송’ 제작기.",
-    etc: "youtube link",
-  },
-  {
-    id: 7,
-    date: "2022",
-    name: "스테이지업",
-    description: "나만의 즐기기에 나선다!",
-    etc: "youtube link",
-  },
-  {
-    id: 8,
-    date: "2022",
-    name: "하이스코어",
-    description:
-      "K-CM송을 차린 KCM이 광고주를 위한 최고의 CM송을  만들어주는 콘텐츠",
-    etc: "youtube link",
-  },
-  {
-    id: 9,
-    date: "2022",
-    name: "서머너즈 워",
-    description:
-      "K-CM송을 차린 KCM이 광고주를 위한 최고의 CM송을  만들어주는 콘텐츠",
-    etc: "youtube link",
-  },
-  {
-    id: 10,
-    date: "2022",
-    name: "다시 만난 세계 : 쩌미문",
-    description:
-      "K-CM송을 차린 KCM이 광고주를 위한 최고의 CM송을  만들어주는 콘텐츠",
-    etc: "youtube link",
-  },
-];
-
 export default FinishProject;
