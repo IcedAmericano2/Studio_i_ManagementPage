87c6e3753156c908898dcb32d188377f00354365
kimhongrae <khr0690@gmail.com>
2023-12-06 00:35:17 +0900

    style: 완료된 프로젝트 진행된 프로젝트와 css 맞추기

--- src/Pages/ProjectPage/FinishProject.js
+++ src/Pages/ProjectPage/FinishProject.js
@@ -23,10 +23,8 @@ const AppContainer = styled.div`
 const Container = styled.div`
   text-align: left;
 `;
-
 const StyledTable = styled.table`
   width: 100%;
-  margin-top: 20px;
   border-collapse: separate;
   border-spacing: 0 16px;
 
@@ -44,17 +42,26 @@ const StyledTable = styled.table`
     background-color: #ffffff;
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
   }
+  tbody tr td {
+    padding: 20px 64px;
+  }
 
   tbody tr:hover {
     background-color: #f5f5f5;
   }
+  tbody tr td:nth-child(3),
+  tbody tr td:nth-child(4) {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 150px;
+  }
 `;
-
 const LabelArea = styled.div`
-  background-color: transparent;
-  border-radius: 32px;
-  border: 2px solid #ff530e;
   width: 128px;
+  background: transparent;
+  border: 2px solid #ff530e;
+  border-radius: 32px;
   text-align: center;
   color: #ff530e;
 `;
@@ -70,6 +77,7 @@ const DeleteButton = styled.button`
     background-color: black;
   }
 `;
+
 const PaginationContainer = styled.div`
   .pagination {
     list-style-type: none;
